c6d5eb8b44107294655f3a7e2c4db049a6aa2153
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2019-06-04 15:55:46 +0900

    change the form from functions to class

--- algorithm/bipartite_matching.cpp
+++ algorithm/bipartite_matching.cpp
@@ -6,47 +6,50 @@
 using namespace std;
 
 // Copy start
-const int MAX_V = 2002; // set your self
-int V;                  // node number
-vector<int> G[MAX_V];   // graph
-int match[MAX_V];       // pair of matching
-bool used[MAX_V];       // flag whether is checked or not by DFS
-
-void add_edge(int u, int v) {
-    G[u].push_back(v);
-    G[v].push_back(u);
-}
-
-bool dfs(int v) {
-    used[v] = true;
-    for (int i = 0; i < G[v].size(); i++) {
-        int u = G[v][i], w = match[u];
-        if (w < 0 || (!used[w] && dfs(w))) {
-            match[v] = u;
-            match[u] = v;
-            return true;
+// time complexity is O(|E||V|)
+class BipartiteMatching {
+    private:
+        int node_size;
+        vector<vector<int> > graph;
+        vector<int> match;
+        vector<bool> is_used;
+    public:
+        BipartiteMatching(int n) {
+            node_size = n;
+            graph.resize(n);
+            match.resize(n);
+            is_used.resize(n);
         }
-    }
-    return false;
-}
-
-// O(|E||V|)
-int bipartite_matching() {
-    int res = 0;
-    memset(match, -1, sizeof(match));
-    for (int v = 0; v < V; v++) {
-        if (match[v] < 0) {
-            memset(used, 0, sizeof(used));
-            if (dfs(v)) {
-                res++;
+        void add_edge(int u, int v) {
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        bool dfs(int v) {
+            is_used[v] = true;
+            for (int i = 0; i < graph[v].size(); i++) {
+                int u = graph[v][i], w = match[u];
+                if (w < 0 || (!is_used[w] && dfs(w))) {
+                    match[v] = u;
+                    match[u] = v;
+                    return true;
+                }
             }
+            return false;
         }
-    }
-    return res;
-}
+        int calc_match() {
+            int cnt = 0;
+            fill(match.begin(), match.end(), -1);
+            for (int v = 0; v < node_size; v++) {
+                if (match[v] < 0) {
+                    fill(is_used.begin(), is_used.end(), false);
+                    if (dfs(v)) cnt++;
+                }
+            }
+            return cnt;
+        }
+};
 // Copy end
 
-int R, C;
 char grid[44][44];
 int number[44][44];
 
@@ -54,6 +57,7 @@ int main() {
     // This sample is AtCoder SoundHound Inc. Programming Contest 2018 C
     cin.tie(0);
     ios::sync_with_stdio(false);
+    int R, C, V = 0;
     cin >> R >> C;
     int now_number = 0;
     for (int i = 0; i < R; i++) {
@@ -65,18 +69,20 @@ int main() {
             }
         }
     }
-    // You don't need to add start and end node
+
+    BipartiteMatching B(V); // initialize with node size
+
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             if (grid[i][j] == '*') continue;
             if (i + 1 < R && grid[i + 1][j] == '.') {
-                add_edge(number[i][j], number[i + 1][j]);
+                B.add_edge(number[i][j], number[i + 1][j]);
             }
             if (j + 1 < C && grid[i][j + 1] == '.') {
-                add_edge(number[i][j], number[i][j + 1]);
+                B.add_edge(number[i][j], number[i][j + 1]);
             }
         }
     }
-    cout << V - bipartite_matching() << endl;
+    cout << V - B.calc_match() << endl;
     return 0;
 }
