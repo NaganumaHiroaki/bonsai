c218c259950fc9bb0f6b2f74cd1be666514e973c
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2019-08-11 11:30:01 +0900

    remove template

--- data_structure/union_find.cpp
+++ data_structure/union_find.cpp
@@ -7,32 +7,27 @@ using namespace std;
 typedef long long ll;
 
 // Copy start
-template <class T>
 class UnionFind {
     private:
-        T node_num;
-        vector<T> parent;
-        vector<T> node_rank;
-        vector<T> sizes;
+        vector<int> parent;
+        vector<int> node_rank;
+        vector<int> sizes;
     public:
-        UnionFind(T n) {
-            node_num = n;
-            parent.resize(node_num);
-            node_rank.resize(node_num);
-            sizes.resize(node_num);
-            for (T i = 0; i < node_num; i++) {
+        UnionFind(int node_num):
+        parent(vector<int>(node_num)), node_rank(vector<int>(node_num)), sizes(vector<int>(node_num)) {
+            for (int i = 0; i < node_num; ++i) {
                 parent[i] = i;
                 node_rank[i] = 0;
                 sizes[i] = 1;
             }
         }
-        T root(T u) {
+        int root(int u) {
             return parent[u] == u ? u : parent[u] = root(parent[u]);
         }
-        bool same(T u, T v) {
+        bool same(int u, int v) {
             return root(u) == root(v);
         }
-        void unite(T u, T v) {
+        void unite(int u, int v) {
             u = root(u);
             v = root(v);
             if (u == v) return;
@@ -48,7 +43,7 @@ class UnionFind {
                 }
             }
         }
-        T get_size(T u) {
+        int get_size(int u) {
             return sizes[root(u)];
         }
 };
@@ -58,7 +53,7 @@ int main() {
     // This sample is Atcoder Beginners Contest 120 D
     ll N, M;
     cin >> N >> M;
-    UnionFind<ll> UF(N);
+    UnionFind uf(N);
     vector<ll> A(M), B(M);
     for (ll i = 0; i < M; i++) {
         cin >> A[i] >> B[i];
@@ -68,12 +63,12 @@ int main() {
     vector<ll> ans;
     for (ll i = M - 1; i >= 0; i--) {
         ans.push_back(now_sizes);
-        if (UF.same(A[i], B[i])) {
+        if (uf.same(A[i], B[i])) {
             continue;
         }
         else {
-            now_sizes -= UF.get_size(A[i]) * UF.get_size(B[i]);
-            UF.unite(A[i], B[i]);
+            now_sizes -= (ll)uf.get_size(A[i]) * (ll)uf.get_size(B[i]);
+            uf.unite(A[i], B[i]);
         }
     }
     reverse(ans.begin(), ans.end());
