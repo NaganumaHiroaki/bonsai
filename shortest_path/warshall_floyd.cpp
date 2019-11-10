fcfe33e2fb5fb5567709469f9896fbbc3535c626
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2019-11-10 19:08:19 +0900

    refactoring and change example

--- shortest_path/warshall_floyd.cpp
+++ shortest_path/warshall_floyd.cpp
@@ -8,65 +8,70 @@ using namespace std;
 template <class T>
 class WarshallFloyd {
     private:
-        T identity;
-        vector<vector<T> > dist;
+        T identity_;
+        vector<vector<T>> dist_;
     public:
-        WarshallFloyd(int node_num, T _identity):
-        identity(_identity), dist(vector<vector<T> >(node_num, vector<T>(node_num, _identity))) {}
-        void add_edge(int from, int to, T cost) {
-            dist[from][to] = cost;
+        WarshallFloyd(int node_num, T identity):
+        identity_(identity), dist_(vector<vector<T>>(node_num, vector<T>(node_num, identity))) {}
+        void addEdge(int from, int to, T cost) {
+            dist_[from][to] = cost;
         }
-        void calc_shortest_path() {
-            int n = (int)dist.size();
-            for (int k = 0; k < n; k++) {
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (dist[i][k] == identity || dist[k][j] == identity) continue;
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+        void calcShortestPath() {
+            int n = (int)dist_.size();
+            for (int k = 0; k < n; ++k) {
+                for (int i = 0; i < n; ++i) {
+                    for (int j = 0; j < n; ++j) {
+                        if (dist_[i][k] == identity_ || dist_[k][j] == identity_) continue;
+                        dist_[i][j] = min(dist_[i][j], dist_[i][k] + dist_[k][j]);
                     }
                 }
             }
         }
-        T get_dist(int from, int to) {
-            return dist[from][to];
+        T getDist(int from, int to) {
+            return dist_[from][to];
         }
 };
 
-const int INF = 1 << 30;
+using ll = long long;
+
+const ll INF = 1LL << 50;
 
 int main() {
-    // This sample is Atcoder Beginners Contest 73 D
-    int N, M, R, A, B, C;
-    cin >> N >> M >> R;
-    vector<int> r(R);
-    for (int i = 0; i < R; i++) {
-        cin >> r[i];
-        r[i]--;
-    }
-    WarshallFloyd<int> warshall(N, INF);
-    for (int i = 0; i < M; i++) {
+    // This sample is Atcoder Beginners Contest 143 E
+    int N, M;
+    ll L;
+    cin >> N >> M >> L;
+    WarshallFloyd<ll> warshall_floyd1(N, INF);
+    int A, B;
+    ll C;
+    for (int i = 0; i < M; ++i) {
         cin >> A >> B >> C;
-        A--, B--;
-        warshall.add_edge(A, B, C);
-        warshall.add_edge(B, A, C);
+        --A, --B;
+        warshall_floyd1.addEdge(A, B, C);
+        warshall_floyd1.addEdge(B, A, C);
     }
-    warshall.calc_shortest_path();
-    vector<int> perm(R);
-    for (int i = 0; i < R; i++) {
-        perm[i] = i;
+    int Q;
+    cin >> Q;
+    vector<int> S(Q), T(Q);
+    for (int i = 0; i < Q; ++i) {
+        cin >> S[i] >> T[i];
+        --S[i], --T[i];
     }
-    int ans = INF;
-    do {
-        int now_dist = 0;
-        for (int i = 1; i < R; i++) {
-            if (warshall.get_dist(r[perm[i - 1]], r[perm[i]]) == INF) {
-                now_dist = INF;
-                break;
+    warshall_floyd1.calcShortestPath();
+    WarshallFloyd<ll> warshall_floyd2(N, INF);
+    for (int i = 0; i < N; ++i) {
+        for (int j = i + 1; j < N; ++j) {
+            if (warshall_floyd1.getDist(i, j) > L) {
+                continue;
             }
-            now_dist += warshall.get_dist(r[perm[i - 1]], r[perm[i]]);
+            warshall_floyd2.addEdge(i, j, 1);
+            warshall_floyd2.addEdge(j, i, 1);
         }
-        ans = min(ans, now_dist);
-    } while (next_permutation(perm.begin(), perm.end()));
-    cout << ans << endl;
+    }
+    warshall_floyd2.calcShortestPath();
+    for (int i = 0; i < Q; ++i) {
+        ll ans = warshall_floyd2.getDist(S[i], T[i]);
+        cout << (ans == INF ? -1 : ans - 1) << endl;
+    }
     return 0;
 }
