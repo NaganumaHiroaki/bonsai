79985f04c46c8f6b2d226cbc650b4a3161f8b562
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2019-11-10 18:48:31 +0900

    refactoring and change example problem

--- shortest_path/bellmanford.cpp
+++ shortest_path/bellmanford.cpp
@@ -3,84 +3,82 @@
 #include<vector>
 // Need end
 using namespace std;
-typedef long long ll;
 
 // Copy start
 template <class T>
 class Bellmanford {
     private:
-        T identity;
         struct edge {
             int from, to;
             T cost;
         };
-        vector<edge> edges;
-        vector<T> dist;
-        vector<bool> in_negative;
-        bool negative_loop;
+        T identity_;
+        vector<edge> edges_;
+        vector<T> dist_;
+        vector<bool> in_negative_;
+        bool negative_loop_;
     public:
-        Bellmanford(int node_num, int edge_num, T _identity):
-        identity(_identity), dist(vector<T>(node_num, _identity)), in_negative(vector<bool>(node_num, false)) {}
-        void add_edge(int from, int to, T cost) {
+        Bellmanford(int node_num, int edge_num, T identity):
+        identity_(identity), dist_(vector<T>(node_num, identity)), in_negative_(vector<bool>(node_num, false)) {}
+        void addEdge(int from, int to, T cost) {
             edge e = {from, to, cost};
-            edges.push_back(e);
+            edges_.push_back(e);
         }
-        void _calc_shortest_path(int start_node) {
-            dist[start_node] = 0;
-            int n = (int)dist.size();
-            int m = (int)edges.size();
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    edge now_edge = edges[j];
-                    if (dist[now_edge.from] != identity && dist[now_edge.to] > dist[now_edge.from] + now_edge.cost) {
-                        dist[now_edge.to] = dist[now_edge.from] + now_edge.cost;
+        void _calcShortestPath(int start_node) {
+            dist_[start_node] = 0;
+            int n = (int)dist_.size();
+            int m = (int)edges_.size();
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    edge now_edge = edges_[j];
+                    if (dist_[now_edge.from] != identity_ && dist_[now_edge.to] > dist_[now_edge.from] + now_edge.cost) {
+                        dist_[now_edge.to] = dist_[now_edge.from] + now_edge.cost;
                         if (i == n - 1) {
-                            negative_loop = true;
-                            in_negative[now_edge.to] = true;
+                            negative_loop_ = true;
+                            in_negative_[now_edge.to] = true;
                         }
                     }
-                    if (in_negative[now_edge.from]) {
-                        in_negative[now_edge.to] = true;
+                    if (in_negative_[now_edge.from]) {
+                        in_negative_[now_edge.to] = true;
                     }
                 }
             }
         }
-        void calc_shortest_path(int start_node) {
-            // for the function "in_negative_loop", run _calc_shortest_path twice
-            _calc_shortest_path(start_node);
-            _calc_shortest_path(start_node);
+        void calcShortestPath(int start_node) {
+            _calcShortestPath(start_node);
+            _calcShortestPath(start_node);
         }
-        bool have_negative_loop() {
-            return negative_loop;
+        bool hasNegLoop() {
+            return negative_loop_;
         }
-        bool in_negative_loop(int node) {
-            // return if node is in negative loop
-            return in_negative[node];
+        bool inNegLoop(int node) {
+            return in_negative_[node];
         }
-        T get_dist(int node) {
-            return dist[node];
+        T getDist(int node) {
+            return dist_[node];
         }
 };
 // Copy end
 
+const int INF = 1 << 30;
+
 int main() {
-    // This sample is Atcoder Beginners Contest 61 D
-    ll N, M;
-    cin >> N >> M;
-    ll a, b, c;
-    Bellmanford<ll> bell(N, M, 1LL << 50);
-    for (int i = 0; i < M; i++) {
-        cin >> a >> b >> c;
-        a--, b--;
-        bell.add_edge(a, b, -c);
+    // This sample is Atcoder Beginners Contest 137 E
+    int N, M, P;
+    cin >> N >> M >> P;
+    Bellmanford<int> bellmanford(N, M, INF);
+    int A, B, C;
+    for (int i = 0; i < M; ++i) {
+        cin >> A >> B >> C;
+        --A, --B;
+        bellmanford.addEdge(A, B, P - C);
     }
-    bell.calc_shortest_path(0);
-    ll ans = -bell.get_dist(N - 1);
-    if (bell.have_negative_loop() && bell.in_negative_loop(N - 1)) {
-        cout << "inf" << endl;
+    bellmanford.calcShortestPath(0);
+    if (bellmanford.inNegLoop(N - 1)) {
+        cout << -1 << endl;
     }
     else {
-        cout << ans << endl;
+        cout << max(-bellmanford.getDist(N - 1), 0) << endl;
     }
     return 0;
 }
