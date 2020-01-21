f1d2d8f4f90906d8f01b97da11e1b04df07cd8d4
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2020-01-21 23:12:44 +0900

    remove assignment operator

--- geometry/polygon_2d.cpp
+++ geometry/polygon_2d.cpp
@@ -12,60 +12,29 @@ using namespace std;
 template<typename T>
 class Polygon2D {
     private:
-        int idx;
-        T x, y;
+        T x_, y_;
+        int idx_;
     public:
-        Polygon2D():x(0), y(0) {}
-        Polygon2D(T _x, T _y):x(_x), y(_y) {}
-        Polygon2D(T _x, T _y, int _idx):x(_x), y(_y), idx(_idx) {}  // for convex-full
-        static T dot(const Polygon2D<T>& p1, const Polygon2D<T>& p2) {
-            return p1.x * p2.x + p1.y * p2.y;
-        }
-        static T det(const Polygon2D<T>& p1, const Polygon2D<T>& p2) {
-            return p1.x * p2.y - p1.y * p2.x;
-        }
-        static T pow_dist(const Polygon2D<T>& p) {
-            return dot(p, p);
-        }
-        const Polygon2D operator=(const Polygon2D<T>& p) {
-            x = p.x;
-            y = p.y;
-            idx = p.idx;
-            return *this;
-        }
-        const Polygon2D operator+(const Polygon2D<T>& p) const {
-            Polygon2D<T> res = *this;
-            res.x += p.x;
-            res.y += p.y;
-            return res;
-        }
-        const Polygon2D operator-(const Polygon2D<T>& p) const {
-            Polygon2D<T> res = *this;
-            res.x -= p.x;
-            res.y -= p.y;
-            return res;
-        }
-        T get_x() const {
-            return x;
-        }
-        T get_y() const {
-            return y;
-        }
-        int get_idx() const {
-            return idx;
-        }
-        void show() {
-            // for debug
-            cout << '(' << x << ',' << y << ')' << endl;
-        }
+        Polygon2D() {}
+        Polygon2D(T x, T y):x_(x), y_(y) {}
+        Polygon2D(T x, T y, int idx):x_(x), y_(y), idx_(idx) {}  // for convex-full
+        static T dot(const Polygon2D<T>& p1, const Polygon2D<T>& p2) {return p1.x_ * p2.x_ + p1.y_ * p2.y_;}
+        static T det(const Polygon2D<T>& p1, const Polygon2D<T>& p2) {return p1.x_ * p2.y_ - p1.y_ * p2.x_;}
+        static T pow(const Polygon2D<T>& p) {return dot(p, p);}
+        const Polygon2D operator+(const Polygon2D<T>& p) const {Polygon2D<T> res = *this; res.x_ += p.x_; res.y_ += p.y_; return res;}
+        const Polygon2D operator-(const Polygon2D<T>& p) const {Polygon2D<T> res = *this; res.x_ -= p.x_; res.y_ -= p.y_; return res;}
+        T getX() const {return x_;}
+        T getY() const {return y_;}
+        int getIdx() const {return idx_;}
+        friend ostream& operator<<(ostream& stream, const Polygon2D& p) {stream << '(' << p.x_ << ',' << p.y_ << ')'; return stream;}
 };
 
 // Graham's scan
 template<typename T>
 vector<Polygon2D<T> > convex_hull(vector<Polygon2D<T> > ps, int vertex_num) {
     sort(ps.begin(), ps.end(), [](const Polygon2D<T>& p, const Polygon2D<T>& q) {
-        if (p.get_x() != q.get_x()) return p.get_x() < q.get_x();
-        return p.get_y() < q.get_y();
+        if (p.getX() != q.getX()) return p.getX() < q.getX();
+        return p.getY() < q.getY();
     });
     int k = 0;
     vector<Polygon2D<T> > qs(vertex_num * 2);
@@ -92,7 +61,7 @@ int main() {
     ios::sync_with_stdio(false);
     int N;
     cin >> N;
-    vector<Polygon2D<double> > ps(N);
+    vector<Polygon2D<double>> ps(N);
     double x, y;
     for (int i = 0; i < N; ++i) {
         cin >> x >> y;
@@ -108,11 +77,11 @@ int main() {
     qs.push_back(qs[0]);
     qs.push_back(qs[1]);
     vector<double> ans(N, 0.0);
-    for (int i = 0; i < qs.size() - 2; i++) {
+    for (int i = 0; i < (int)qs.size() - 2; i++) {
         Polygon2D<double> pr = qs[i] - qs[i + 1], pq = qs[i + 2] - qs[i + 1];
-        double len_pr = Polygon2D<double>::pow_dist(pr);
-        double len_pq = Polygon2D<double>::pow_dist(pq);
-        ans[qs[i + 1].get_idx()] = (PI - acos(Polygon2D<double>::dot(pr, pq) / sqrt(len_pr * len_pq))) / (2 * PI);
+        double len_pr = Polygon2D<double>::pow(pr);
+        double len_pq = Polygon2D<double>::pow(pq);
+        ans[qs[i + 1].getIdx()] = (PI - acos(Polygon2D<double>::dot(pr, pq) / sqrt(len_pr * len_pq))) / (2 * PI);
     }
     cout << fixed << setprecision(7);
     for (int i = 0; i < N; i++) {
