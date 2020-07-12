eed8c5175695e85b3c91bfd8f1de4174ea1c87c7
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2020-07-12 20:39:41 +0900

    refactor

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -4,65 +4,69 @@
 #include<cassert>
 #include<vector>
 // Need end
-using namespace std;
 
 // Copy start
 template<typename ITERATOR>
 class KMP {
-    private:
-        ITERATOR pattern;  // iterator
-        vector<int> shift_table;
-    public:
-        KMP(ITERATOR _pattern):pattern(_pattern) {
-            // calculate tagged border
-            vector<int> tagged_border(pattern.size() + 1);
-            tagged_border[0] = -1;  // -1 means that there is no border
-            int prev_border = -1;
-            for (int i = 0; i < pattern.size(); ++i) {
-                while (prev_border >= 0 && pattern[prev_border] != pattern[i]) {
-                    prev_border = tagged_border[prev_border];
-                }
-                prev_border++;
-                if (i + 1 < pattern.size() && pattern[i + 1] == pattern[prev_border]) {
-                    tagged_border[i + 1] = tagged_border[prev_border];
-                }
-                else tagged_border[i + 1] = prev_border;
+private:
+    ITERATOR pattern;  // iterator
+    std::vector<int> shift_table;
+public:
+    KMP(ITERATOR _pattern):pattern(_pattern) {
+        // calculate tagged border
+        std::vector<int> tagged_border(pattern.size() + 1);
+        tagged_border[0] = -1;  // -1 means that there is no border
+        int prev_border = -1;
+        for (int i = 0; i < (int)pattern.size(); ++i) {
+            while (prev_border >= 0 && pattern[prev_border] != pattern[i]) {
+                prev_border = tagged_border[prev_border];
             }
-            shift_table = tagged_border;
+            prev_border++;
+            if (i + 1 < (int)pattern.size() && pattern[i + 1] == pattern[prev_border]) {
+                tagged_border[i + 1] = tagged_border[prev_border];
+            } else {
+                tagged_border[i + 1] = prev_border;
+            }
         }
-        vector<int> get_shift_table() {
-            return shift_table;
-        }
-        vector<int> search(ITERATOR target) {
-            assert(target.size() >= pattern.size());
-            // return matching position
-            vector<int> res;
-            int target_size = target.size(), pattern_size = pattern.size();
-            int target_ptr = 0, pattern_ptr = 0;
-            while (target_ptr < target_size) {
-                if (target[target_ptr] == pattern[pattern_ptr]) {
+        shift_table = tagged_border;
+    }
+    std::vector<int> getShiftTable() {
+        return shift_table;
+    }
+    std::vector<int> search(ITERATOR target) {
+        assert(target.size() >= pattern.size());
+        // return matching position
+        std::vector<int> res;
+        int target_size = (int)target.size();
+        int pattern_size = (int)pattern.size();
+        int target_ptr = 0;
+        int pattern_ptr = 0;
+        while (target_ptr < target_size) {
+            if (target[target_ptr] == pattern[pattern_ptr]) {
+                target_ptr++;
+                pattern_ptr++;
+            } else {
+                if (pattern_ptr == 0) {
                     target_ptr++;
-                    pattern_ptr++;
-                }
-                else {
-                    if (pattern_ptr == 0) target_ptr++;
-                    else {
-                        if (shift_table[pattern_ptr] < 0) {
-                            pattern_ptr = 0;
-                        }
-                        else pattern_ptr = shift_table[pattern_ptr];
-                    }
-                }
-                if (pattern_ptr == pattern_size) {
-                    res.push_back(target_ptr - pattern_ptr);
+                } else {
                     if (shift_table[pattern_ptr] < 0) {
                         pattern_ptr = 0;
+                    } else {
+                        pattern_ptr = shift_table[pattern_ptr];
                     }
-                    else pattern_ptr = shift_table[pattern_ptr];
                 }
             }
-            return res;
+            if (pattern_ptr == pattern_size) {
+                res.push_back(target_ptr - pattern_ptr);
+                if (shift_table[pattern_ptr] < 0) {
+                    pattern_ptr = 0;
+                } else {
+                    pattern_ptr = shift_table[pattern_ptr];
+                }
+            }
         }
+        return res;
+    }
 };
 // Copy end
 
@@ -95,6 +99,8 @@ void dfs(int idx) {
     if (period[next_idx] >= 0) period[idx] = period[next_idx] + 1;
 }
 
+using namespace std;
+
 int main() {
     // This sample is Atcode Beginners Contest 135 F
     cin.tie(0);
@@ -109,7 +115,7 @@ int main() {
     N = SS.size();
     KMP<string> kmp(T);
     vector<int> result = kmp.search(SS + SS);
-    for (int i = 0; i < result.size(); ++i) {
+    for (int i = 0; i < (int)result.size(); ++i) {
         is_ok[result[i]] = true;
     }
     for (int i = 0; i < N; ++i) {
