8594de576f631d264a2dd6db442ecf5547da0495
naganuma hiroaki <hiroaki.naganuma.t8@dc.tohoku.ac.jp>
2020-09-17 19:25:51 +0900

    refactor

--- data_structure/bit.cpp
+++ data_structure/bit.cpp
@@ -14,10 +14,10 @@ class BIT {
     // 1-indexed
     private:
         size_t node_size_;
-        vector<T> node_;
         T identity_;
         function<T(T, T)> operation_;
         function<T(T, T)> update_;
+        vector<T> node_;
     public:
         BIT(size_t node_size, T identity, function<T(T, T)> operation, function<T(T, T)> update):
         node_size_(node_size), identity_(identity), operation_(operation), update_(update), node_(vector<T>(node_size + 1, identity)) {}
